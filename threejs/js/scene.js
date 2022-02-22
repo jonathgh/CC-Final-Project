208bb0adfe528116d9cf4f9914597b393b1cdd24
jonathgh <jonathangho@protonmail.com>
2022-02-22 01:36:41 +0100

    updated files

--- threejs/js/scene.js
+++ threejs/js/scene.js
@@ -33,6 +33,68 @@ mesh.castShadow = true;
 // add the mesh to the scene
 scene.add(mesh);
 
+//credit: https://tympanus.net/codrops/2019/01/17/interactive-particles-with-three-js/
+
+//particles are a simple quad, formed by 4 vertices and 2 triangles.
+const geometry = new THREE.InstancedBufferGeometry();
+
+// positions
+const positions = new THREE.BufferAttribute(new Float32Array(4 * 3), 3);
+positions.setXYZ(0, -0.5, 0.5, 0.0);
+positions.setXYZ(1, 0.5, 0.5, 0.0);
+positions.setXYZ(2, -0.5, -0.5, 0.0);
+positions.setXYZ(3, 0.5, -0.5, 0.0);
+geometry.addAttribute('position', positions);
+
+// uvs
+const uvs = new THREE.BufferAttribute(new Float32Array(4 * 2), 2);
+uvs.setXYZ(0, 0.0, 0.0);
+uvs.setXYZ(1, 1.0, 0.0);
+uvs.setXYZ(2, 0.0, 1.0);
+uvs.setXYZ(3, 1.0, 1.0);
+geometry.addAttribute('uv', uvs);
+
+// index
+geometry.setIndex(new THREE.BufferAttribute(new Uint16Array([ 0, 2, 1, 2, 3, 1 ]), 1));
+
+//loop through the pixels of the image and assign our instanced attributes.
+const indices = new Uint16Array(this.numPoints);
+const offsets = new Float32Array(this.numPoints * 3);
+const angles = new Float32Array(this.numPoints);
+
+for (let i = 0; i < this.numPoints; i++) {
+	offsets[i * 3 + 0] = i % this.width;
+	offsets[i * 3 + 1] = Math.floor(i / this.width);
+
+	indices[i] = i;
+
+	angles[i] = Math.random() * Math.PI;
+}
+
+geometry.addAttribute('pindex', new THREE.InstancedBufferAttribute(indices, 1, false));
+geometry.addAttribute('offset', new THREE.InstancedBufferAttribute(offsets, 3, false));
+geometry.addAttribute('angle', new THREE.InstancedBufferAttribute(angles, 1, false));
+
+const uniforms = {
+	uTime: { value: 0 },
+	uRandom: { value: 1.0 },
+	uDepth: { value: 2.0 },
+	uSize: { value: 0.0 },
+	uTextureSize: { value: new THREE.Vector2(this.width, this.height) },
+	uTexture: { value: this.texture },
+	uTouch: { value: null }
+};
+
+const material = new THREE.RawShaderMaterial({
+	uniforms,
+	vertexShader: glslify(require('../../../shaders/particle.vert')),
+	fragmentShader: glslify(require('../../../shaders/particle.frag')),
+	depthTest: false,
+	transparent: true
+});
+
+//end credit/////////
+
 // LIGHTING
 // AMBIENT
 let ambientLight = new THREE.AmbientLight(0xffffff, 0.8); // (color, intensity)
